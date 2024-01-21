3e87cd72e6573d02bed64d26acd85f2e90b33378
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2024-01-21 14:23:09 +0800

    bruh

--- acestudio.py
+++ acestudio.py
@@ -4,27 +4,18 @@ import os
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    'channel': 'web',
+    'device': 'web',
+    'platform': 'web',
+    'Version': 'web'
 }
 
-data = {
-    'mobileCountry': '886',
-    'mobile': '0966513808',
-    'birthday': '20040803'
-}
+url = 'https://as-api.tdacestudio.com/api/as/web/auth/code?phone=886-0966513808'
 
-url = 'https://group.openpoint.com.tw/auth/service/ajax/login/forget-pwd/send-sms-code'
-
-response = requests.post(url, headers=headers, data=data)
+response = requests.get(url, headers=headers)
 if response.status_code == requests.codes.ok:
     j = response.json()
     print(j)
-    if j['codeId'] == '200':
-        with open('t.txt', 'r') as f:
-            n = int(f.read())
-
-        with open('t.txt', 'w') as f:
-            f.write(str(n+1))
 
 with open('log.txt', 'a', encoding='utf-8') as f:
     t = datetime.now(timezone(timedelta(hours=+8))).strftime('%Y-%m-%d %H:%M:%S')
